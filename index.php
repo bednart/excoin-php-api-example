5d511d44e20e83cfc12b8a8bdf284f96e82136ff
Tomáš Bednár <bednar.tomas.beda@gmail.com>
2015-01-03 21:50:13 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+// include excoin class
+include_once ('excoin.php');
+
+// url
+$url = 'https://api.exco.in/v1/account/summary';
+
+// keys
+$apikey = 'API_KEY';
+$apisecret = 'API_SECRET';
+        
+// create class
+$excoin = new Excoin($url, $apikey, $apisecret);
+
+// get JSON objects
+$nonce = $excoin->getNonceRequest();
+$timestamp = $excoin->getTimestampRequest();
+
+// print nonce result
+echo '<h1>Excoin API nonce request result:</h1>';
+echo '<pre>';
+print_r($nonce);
+echo '</pre>';
+
+echo '<br/><br/>';
+
+// print timestamp result
+echo '<h1>Excoin API timestamp request result:</h1>';
+echo '<pre>';
+print_r($nonce);
+echo '</pre>';
+
+?>
